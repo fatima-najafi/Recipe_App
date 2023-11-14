680608366a1fbe9d789f47891133eea25e1bb600
Fatima Najafi <najafifatima762@gmail.com>
2023-11-14 13:39:36 +0430

    Add user authentation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+  validates :name, presence: true
+  validates :email, presence: true
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
