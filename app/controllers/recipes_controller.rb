ce1ab12f1df5adb091b1ad99c45029fb053eae4e
jecode93 <jeanemmanuelcadet@gmail.com>
2023-11-14 10:08:55 -0500

    Create recipe list and recipe details

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,11 +1,43 @@
 class RecipesController < ApplicationController
-  def index; end
+  def index
+    @recipes = current_user.recipes
+  end
 
-  def show; end
+  def show
+    @recipe = Recipe.includes(:foods).find(params[:id])
+    @foods = @recipe.foods
+  end
 
-  def new; end
+  def new
+    @recipe = Recipe.new
+  end
 
-  def create; end
+  def create
+    @recipe = current_user.recipes.new(recipe_params)
+    
+    if @recipe.save
+      redirect_to recipes_path, notice: 'Recipe was created successfully'
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
 
-  def destroy; end
+  def destroy
+    @recipe = current_user.recipes.find(params[:id])
+
+    if @recipe.destroy
+      redirect_to recipes_path, notice: 'Recipe deleted successfully'
+    else
+      redirect_to recipes_path, notice: 'Recipe cannot delete'
+    end
+  end
+  
+
+  private
+  
+  def recipe_params
+    params.require(:recipe).permit(:name, :preparation_time, :cooking_time, :description)
+  end
+
+  
 end
