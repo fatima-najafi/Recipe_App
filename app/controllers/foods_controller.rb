40894e901b99ba77115317699899b2cc170d7c4a
Fatima Najafi <najafifatima762@gmail.com>
2023-11-14 18:01:59 +0430

    Display the food list

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -1,9 +1,32 @@
 class FoodsController < ApplicationController
-  def index; end
+def index
+    @foods = current_user.foods
+  end
 
-  def new; end
+  def new
+    @food = Food.new
+  end
 
-  def create; end
+  def create
+    @food = current_user.foods.new(food_params)
+    if @food.save
+      redirect_to foods_url, notice: 'Food item was successfully created.'
+    else
+      flash.now[:alert] = 'Food item could not be created.'
+      render :new
+    end
+  end
 
-  def destroy; end
+  def destroy
+    @food = Food.find(params[:id])
+    @food.destroy
+    redirect_to foods_url, notice: 'Food item was successfully deleted.'
+  end
+
+  private
+
+  # Define permitted parameters for creating a new food item
+  def food_params
+    params.require(:food).permit(:name, :measurement_unit, :price, :quantity)
+  end
 end
