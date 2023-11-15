acc708c04f301098b7fc46e2819ccafa57b65882
Fatima Najafi <najafifatima762@gmail.com>
2023-11-15 18:17:02 +0430

    Update the routes and database

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,18 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   devise_for :users
-  root "foods#index"
-  # root "recipes#public_recipes"
-  get "users/general_shopping_list"
-  
-  resources :foods, only: [:index, :new, :create, :destroy]
-  resources :recipes, only: [:index, :show, :new, :create, :destroy] do
-    resources :recipe_foods, only: [:new, :create, :destroy]
-  end
-    # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
-  get "up" => "rails/health#show", as: :rails_health_check
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "posts#index"
+   root "foods#index"
+get "users/general_shopping_list"
+  resources :foods, except: :update
+  resources :recipes, except: :update do
+    member do
+      put :toggle_public
+    end
+    resources :recipe_foods, only: [:index, :new, :create, :destroy]
+  end
+  get '/public_recipes', to: 'public_recipes#index', as: 'public_recipes'
+  # get '/general_shopping_list', to: 'shopping_list#index', as: 'general_shopping_list'
 end
+
