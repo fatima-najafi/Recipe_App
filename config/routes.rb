374144ca93b1c4c1d9f59133d8dc1176107fa2b2
Fatima Najafi <najafifatima762@gmail.com>
2023-11-15 18:35:54 +0430

    Update

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
    root "foods#index"
 get "users/general_shopping_list"
   resources :foods, except: :update
@@ -13,6 +10,5 @@ get "users/general_shopping_list"
     resources :recipe_foods, only: [:index, :new, :create, :destroy]
   end
   get '/public_recipes', to: 'public_recipes#index', as: 'public_recipes'
-  # get '/general_shopping_list', to: 'shopping_list#index', as: 'general_shopping_list'
 end
 
